2290a43c100194780c04ea643344fdf43458dbe9
Vilhelmina <90849312+vilmelhina@users.noreply.github.com>
2022-03-03 15:49:53 +0100

    score implementation not working yet

--- gamecommands.c
+++ gamecommands.c
@@ -17,11 +17,45 @@ int checkGameOver(){ /*Checks if a block already occupies the drop area, indicat
 void pieceDropped()
 {
     writeToBackground(foreground, background);
+    if (lineFull(tetrominoCoord[0])) {
+        ledContent(0xCC);
+        clearLine(tetrominoCoord[0]);
+    }
     if (checkGameOver()){
-        ledContent(0xff); //TODO: Something useful        
+        ledContent(0xaa);
+        while(1); //TODO: Something useful
     }
     updateScore();
     updatePulse(); // should be moved when pulse shit is implemented
     getPiece();
     getNextPiece();
+}
+
+int lineFull(int line) {
+    int c;
+    for (c = 2; c < PIXELCOLUMNS - 1; c++) {
+        if (!background[line][c]) return 0;
+    }
+    return 1;
+}
+
+void clearLine(int line) {
+    int c, r, i;
+    for (c = 2; c < PIXELCOLUMNS - 4; c++) {
+        for (r = -1; r < 2; r++)
+            background[line + r][c] = 0;
+    }
+    for (r = line; r > GAMEBOARDSTART + 4; r--) {
+        for (c = 2; c < PIXELCOLUMNS - 1; c++) {
+            background[r - 1][c] = background[r-3 - 1][c];
+            background[r][c] = background[r-3][c];
+            background[r + 1][c] = background[r-3 + 1][c];
+        }
+    }
+    for (i = 2; i < PIXELCOLUMNS - 2; i++) {
+        background[GAMEBOARDSTART + 1][c] = 0;
+        background[GAMEBOARDSTART + 2][c] = 0;
+        background[GAMEBOARDSTART + 3][c] = 0;
+    }
+    score++;
 }
