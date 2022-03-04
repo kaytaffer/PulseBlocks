934ddcda53edb63c4fa0f34ab057da80dce22669
Vilhelmina <90849312+vilmelhina@users.noreply.github.com>
2022-03-04 14:44:11 +0100

    changed indentation

--- interrupthandler.c
+++ interrupthandler.c
@@ -13,88 +13,64 @@ int deltaSpeed = 0;
 int gameSpeed = 0;
 /* Interrupt Service Routine  Handles all interrupts from I/O; switches, timers and the like. */
 void userISR() {
-  int moveLeft = 0;
-  int moveRight = 0;
+    int moveLeft = 0;
+    int moveRight = 0;
 
-  if(IFS(0) & 0x0100) {  //timer 2 interrupt
-    blockFallQuotient++;
-    buttonquotient++;
-    ticks++;
-    deltaSpeed++;
-    IFSCLR(0) = 0x0100; // Resets the interrupt flag for timer 2 to 0.
-      if (ticks == 0xFF) ticks = 0;
-  }
+    if(IFS(0) & 0x0100) {  //timer 2 interrupt
+        blockFallQuotient++;
+        buttonquotient++;
+        ticks++;
+        deltaSpeed++;
+        IFSCLR(0) = 0x0100; // Resets the interrupt flag for timer 2 to 0.
+        if (ticks == 0xFF) ticks = 0;
+    }
 
-  if (getButtons()){
-    pressedButton = getButtons();
-  }
-    
+    if (getButtons())
+        pressedButton = getButtons();
+
     // BUTTONS WHILE NOT IN GAME:
-    if(pressedButton == 0b0001 && !gameInProgress) {
-      gameInProgress = 1;
-    }
-    if(pressedButton == 0b0010 && !gameInProgress) {
-      gameInProgress = 2;
-    }
-    if(pressedButton == 0b0100 && !gameInProgress) {
-      gameInProgress = 0;
-    }
-    if(pressedButton == 0b1000 && !gameInProgress) {
-      gameInProgress = 0;
-    }
-    
-    // BUTTON WHILE IN GAME:
-  if((buttonquotient == 15)){
-    if(pressedButton & 0b1000) //BTN 1
-      leftMove(foreground, PIXELMOVEAMOUNT);
-    if(pressedButton & 0b100){
-      while(falling(foreground, 1)); //BTN2: Hard drop: Makes elements in an array fall until one hits something
-      pieceDropped();
-      delay(100);
-    }
-    if(pressedButton & 0b10)
-      rotate(foreground);
-      delay(100);
-    if(pressedButton & 0b1)
-      rightMove(foreground, PIXELMOVEAMOUNT);       
-    convertPixels(foreground, background, display);
-    displayImage(0, display);
-    pressedButton = 0;
-    buttonquotient = 0;
-// showInt(tetrominoCoord[0], 37, 0, background);//TODO, remove these testers when done. 
-  }
+    if(pressedButton == 0b0001 && !gameInProgress)
+        gameInProgress = 1;
+    if(pressedButton == 0b0010 && !gameInProgress)
+        gameInProgress = 2;
+    if(pressedButton == 0b0100 && !gameInProgress)
+        gameInProgress = 0;
+    if(pressedButton == 0b1000 && !gameInProgress)
+        gameInProgress = 0;
 
-  if(blockFallQuotient > (30 - gameSpeed)) {
-    blockFallQuotient = 0;
-    if(!falling(foreground, 1)){                 
-      pieceDropped();
+    // BUTTON WHILE IN GAME:
+    if((buttonquotient == 15)) {
+        if(pressedButton & 0b1000) //BTN 1
+            leftMove(foreground, PIXELMOVEAMOUNT);
+        if(pressedButton & 0b100){
+            while(falling(foreground, 1)); //BTN2: Hard drop: Makes elements in an array fall until one hits something
+            pieceDropped();
+            delay(100);
+        }
+        if(pressedButton & 0b10)
+            rotate(foreground);
+            delay(100);
+        if(pressedButton & 0b1)
+            rightMove(foreground, PIXELMOVEAMOUNT);
+        convertPixels(foreground, background, display);
+        displayImage(0, display);
+        pressedButton = 0;
+        buttonquotient = 0;
     }
-    convertPixels(foreground, background, display);
-    displayImage(0, display);
-    
-  }
+
+    if(blockFallQuotient > (30 - gameSpeed)) {
+        blockFallQuotient = 0;
+        if(!falling(foreground, 1)){
+          pieceDropped();
+        }
+        convertPixels(foreground, background, display);
+        displayImage(0, display);
+    }
 
     if(deltaSpeed == 100) {   //timing related gamespeed modifier
-    deltaSpeed = 0;
-    if ((gameSpeed < MAXGAMESPEED) && (gameMode == 1))
-      gameSpeed++;
-  }
- /* Unused switch interrupts
-  if(IFS(0) & 0x080000){ //Switch 4 interrupt
-    IFSCLR(0) = 0x080000; // Resets the interrupt flag for SW4 to 0. 
-  }
-
-  if(IFS(0) & 0x008000){ //Switch 3 interrupt
-    IFSCLR(0) = 0x008000; // Resets the interrupt flag for SW3 to 0. 
-  }
-
-  if(IFS(0) & 0x000800){ //Switch 2 interrupt
-    IFSCLR(0) = 0x000800; // Resets the interrupt flag for SW2 to 0. 
-  }
-
-  if(IFS(0) & 0x000080){ //Switch 1 interrupt
-    IFSCLR(0) = 0x000080; // Resets the interrupt flag for SW1 to 0. 
-  }
-*/
+        deltaSpeed = 0;
+        if ((gameSpeed < MAXGAMESPEED) && (gameMode == 1))
+          gameSpeed++;
+    }
 }
 
